05fb66fc47cd4849eef7798e487400829183cd77
guessi <guessi@gmail.com>
2022-12-27 23:37:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Taiwan Holidays
+
+Simple script to retrieve Taiwan Holidays
+
+## Usage
+
+```bash
+bash ./retrieve-taiwan-holidays.sh
+```
+
+## LICENSE
+
+[Unlicense](LICENSE)
---
+++ retrieve-taiwan-holidays.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Data Source: https://data.gov.tw/dataset/123662
+
+# Data extraction logic:
+#
+#   1. fetch original csv input from open data
+#   2. remove data before 2023 and regular holidays (non-adjusted holidays on Saturday or Sunday)
+#   3. convert CSV to JSON with Python scripts
+#   4. prettify JSON output with jq
+
+curl -fsSL "https://data.ntpc.gov.tw/api/datasets/308dcd75-6434-45bc-a95f-584da4fed251/csv/file" | \
+  sed -e '/201[0-9]/d' -e '/202[0-2]/d' -e '/星期六、星期日/d' | \
+  python3 -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' | \
+  jq '.'
