4eb1b9dc372ccae72326244daa6b0dec6ef3efb1
guessi <guessi@gmail.com>
2025-01-06 01:45:55 +0800

    Update for 2025

--- retrieve-taiwan-holidays.sh
+++ retrieve-taiwan-holidays.sh
@@ -5,11 +5,11 @@
 # Data extraction logic:
 #
 #   1. fetch original csv input from open data
-#   2. remove data before 2023 and regular holidays (non-adjusted holidays on Saturday or Sunday)
+#   2. remove data before 2025 and regular holidays (non-adjusted holidays on Saturday or Sunday)
 #   3. convert CSV to JSON with Python scripts
 #   4. prettify JSON output with jq
 
 curl -fsSL "https://data.ntpc.gov.tw/api/datasets/308dcd75-6434-45bc-a95f-584da4fed251/csv/file" | \
-  sed -e '/201[0-9]/d' -e '/202[0-2]/d' -e '/星期六、星期日/d' | \
+  sed -e '/201[0-9]/d' -e '/202[0-4]/d' -e '/星期六、星期日/d' | \
   python3 -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' | \
   jq '.'
